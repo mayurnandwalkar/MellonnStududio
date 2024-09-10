345313d4c8d9bdb795d7d9ddcbe7bf28b72bcf58
mayurnandwalkar <mayur@owowtalents.comgit>
2024-09-10 22:03:30 +0530

    code upated

--- src/Components/LeadersComponent.jsx
+++ src/Components/LeadersComponent.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import SatyamImage from '../assets/Images/Component20.png'
-import GangeshImage from '../assets/Images/Component21.png'
+import SatyamImage from '../assets/Images/satyamImage.png'
+import GangeshImage from '../assets/Images/gangeshImage.png'
+import { FaLinkedin } from "react-icons/fa";
+
 const LeadersComponent = () => {
   return (
     <>
@@ -11,8 +13,18 @@ const LeadersComponent = () => {
             <p className='mt-4'>Our guiding pillars who support us in everything we do.</p>
           </div>
           <div className='display-founder-images mt-5'>
-            <div><img src={SatyamImage} className='img-fluid' /></div>
-            <div><img src={GangeshImage} className='img-fluid' /></div>
+            <div className='text-center'>
+              <img src={SatyamImage} className='img-fluid' />
+              <h4 className='mt-2'>satyam tiwari</h4>
+              <a href="https://www.linkedin.com/in/satyam-tiwari-965598153/" target="_blank" className="visit-linkedin"><FaLinkedin /> Visit LinkedIn</a>
+              <span>founder & ceo</span>
+            </div>
+            <div className='text-center'>
+              <img src={GangeshImage} className='img-fluid' />
+              <h4 className='mt-2'>gangesh pathak</h4>
+              <span>Mentor to founder</span>
+              <a href="https://www.linkedin.com/in/gkp/" target="_blank" className="visit-linkedin"><FaLinkedin /> Visit LinkedIn</a>
+            </div>
           </div>
         </div>
       </section>
